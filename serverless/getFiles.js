7c7188f0a312a33019075fb4880731adfffd52be
precious <ezechukwuifeoma279@gmail.com>
2025-02-03 13:08:25 +0100

    netlifyy8

--- serverless/getFiles.js
+++ serverless/getFiles.js
@@ -2,12 +2,12 @@ const fs = require('fs');
 const path = require('path');
 
 exports.handler = async (event, context) => {
-    console.log('Function invoked');
-    const directoryPath = path.join(__dirname, '..', 'static', 'files'); // Pointing to static/files
+    console.log('Welcome'); // This will log when the function is accessed
+    const directoryPath = path.join(__dirname, '..', 'static', 'files'); // Path to static/files
     console.log('Directory path:', directoryPath);
 
     try {
-        const files = fs.readdirSync(directoryPath); // Reading files in the directory
+        const files = fs.readdirSync(directoryPath); // Read files in the directory
         console.log('Files found:', files);
 
         const downloadableFiles = files.map(file => ({
